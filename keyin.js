c3cb86a3925d10331886bb372613f64244c37f4b
Yiying Tao <a13918925985@gmail.com>
2020-10-22 17:37:03 -0400

    Keyboard added. Food and Quantity entry finished.

---
+++ keyin.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('input#foodinput').mlKeyboard({layout: 'en_US'});
+});
+
+$(document).ready(function(){
+  $('input#quantityinput').mlKeyboard({layout: 'en_US'});
+});
