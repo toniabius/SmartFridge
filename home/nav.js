ffe2c86831788bc87762b2b701ff756dd6e74b41
Yiying Tao <a13918925985@gmail.com>
2020-11-09 00:08:24 -0500

    recipe added

---
+++ home/nav.js
@@ -0,0 +1,15 @@
+      function back() {
+        window.open("");
+      }
+      function openHome() {
+        window.open("../home/homepage.html");
+      }
+      function openOrder() {
+        window.open("../order/order.html");
+      }
+      function openRecipe() {
+        window.open("../recipe/recipe.html");
+      }
+      function openSetting() {
+        window.open("../setting/setting.html");
+      }
