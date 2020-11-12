fbd0b1ffbf19ae9b5a8b04ee42a8dbe3f96359f3
SOO OH <soooh@SOOs-MBP.fios-router.home>
2020-11-12 16:35:07 -0500

    Fix nav bar, add tablet size border, modify home

--- setting/setting.js
+++ setting/setting.js
@@ -51,20 +51,3 @@ window.onload = function(){
         }
     }
 }
-
-// nav bar
-function back() {
-    window.open("", "_self");
-}
-function openHome() {
-    window.open("../home/homepage.html", "_self");
-}
-function openOrder() {
-    window.open("../order/order.html", "_self");
-}
-function openRecipe() {
-    window.open("../recipe/recipe.html", "_self");
-}
-function openSetting() {
-    window.open("../setting/setting.html", "_self");
-} 
