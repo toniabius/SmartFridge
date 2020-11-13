bd3e4f39007f77b3cf2d0a6e8f4dee2a08d45fec
kdo6766 <54635401+kdo6766@users.noreply.github.com>
2020-11-12 21:14:25 -0500

    changing some things

--- home/nav.js
+++ home/nav.js
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-function openGroceryList() {
-  window.open("../grocery/grocery.html", "_self");
-}
-function openHome() {
-  window.open("../home/homepage.html", "_self");
-}
-function openOrder() {
-  window.open("../order/order.html", "_self");
-}
-function openRecipe() {
-  window.open("../recipe/recipe.html", "_self");
-}
-function openSetting() {
-  window.open("../setting/setting.html", "_self");
-}
-function openGroceryList() {
-  window.open("../grocery/grocery.html", "_self");
-}
-function checkout() {
-  window.open("../order/checkout.html", "_self");
-}
-=======
       function openGroceryList() {
         window.open("../grocery/grocery.html", "_self");
       }
@@ -37,9 +14,8 @@ function checkout() {
          window.open("../setting/setting.html", "_self");
       }
       function openGroceryList() {
-        window.open("../order/phoneconnect.html","_self");
+        window.open("../order/grocery.html","_self");
       }
       function checkout(){
         window.open("../order/checkout.html","_self");
       }
->>>>>>> Stashed changes
